7cc50008ab345cf320fe2c2ccdab2177f53e45c6
pavel.liashkov <pavel.liashkov@exness.com>
2021-01-09 17:42:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import pathlib
 from importlib.machinery import SourceFileLoader
 
 from setuptools import find_packages, setup
@@ -6,13 +7,18 @@ from setuptools import find_packages, setup
 version = SourceFileLoader("version", "starlette-i18n/version.py").load_module()
 
 
+_ROOT = pathlib.Path(__file__).parent
+
+with open(str(_ROOT / "README.rst")) as f:
+    readme = f.read()
+
 CLASSIFIERS = [
-    "Development Status :: 3 - Beta",
-    "Programming Language :: Python",
+    "Development Status :: 5 - Production/Stable",
+    "Programming Language :: Python :: 3 :: Only",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
-    "Operating System :: OS Independent",
+    "Operating System :: POSIX",
     "Topic :: System :: Networking",
     "Topic :: Software Development",
     "Topic :: Software Development :: Libraries",
@@ -21,13 +27,21 @@ CLASSIFIERS = [
 ]
 
 setup(
+    name="starlette-i18n",
+    version=str(version.VERSION),
+    license="Apache License, Version 2.0",
+    description="Localisation helper for starlette",
+    long_description=readme,
+    long_description_content_type="text/x-rst",
     author="Pavel Liashkov",
     author_email="pavel.liashkov@protonmail.com",
-    name="starlette-i18n",
-    description="Localisation helper for starlette",
-    version=str(version.VERSION),
+    maintainer="Pavel Liashkov",
+    maintainer_email="pavel.liashkov@protonmail.com",
+    download_url="https://pypi.python.org/pypi/starlette-i18n",
     url="https://github.com/bigbag/starlette-i18n",
-    platforms=CLASSIFIERS,
+    platforms=["POSIX"],
+    classifiers=CLASSIFIERS,
+    python_requires=">=3.7",
     install_requires=["starlette==0.14.1", "babel==2.9.0"],
     packages=find_packages(),
     include_package_data=True,
