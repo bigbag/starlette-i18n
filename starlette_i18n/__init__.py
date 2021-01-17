6b746a4bfbb33f951334a9c0c2b3f9b5066fbc8a
Pavel Liashkov <pavel.liashkov@protonmail.com>
2021-01-17 18:45:35 +0200

    Update README

--- starlette_i18n/__init__.py
+++ starlette_i18n/__init__.py
@@ -1,7 +1,10 @@
+from .constants import DEFAULT_LOCALE, LANGUAGE_HEADER
 from .i18n import get_locale, gettext_lazy, load_gettext_translations, set_locale
 from .middleware import LocaleMiddleware
 
 __all__ = [
+    "DEFAULT_LOCALE",
+    "LANGUAGE_HEADER",
     "LocaleMiddleware",
     "gettext_lazy",
     "get_locale",
